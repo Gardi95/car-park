c14309ac97323d9c38be5a1841e078de5437e3b2
Mohammed Yaba <91971316+Gardi95@users.noreply.github.com>
2022-06-26 22:46:13 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/car_park.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/car_park.dir/main.cpp.obj"
+  "car_park.exe"
+  "car_park.exe.manifest"
+  "car_park.pdb"
+  "libcar_park.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/car_park.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
